4e01465ca4174812765ddd4a123924d5755e9f71
serg-bolshakov <you@example.com>
2022-11-12 14:36:15 +0300

    Проверка доступа

---
+++ appl.js
@@ -0,0 +1,44 @@
+const userOne = {
+    role: 'admin',
+    firstname: 'Дмитрий',
+}
+
+const userTwo = {
+    role: 'manager',
+    firstname: 'Артем',
+}
+
+function checkAccess (user, role) {
+    if (user.role === role) {
+        return true;
+    } 
+    return false;
+    }
+
+function showSuccessMessage (user) {
+    console.log ('#');
+    console.log (`Доступ открыт ${user.firstname}`);
+    console.log ('#');
+}
+
+function showErrorMessage (user) {
+    console.log ('#');
+    console.log (`${user.firstname} - Доступ закрыт`);
+    console.log ('#');
+}
+if (checkAccess(userOne, 'noadmin')) {
+    showSuccessMessage(userOne);
+} else {
+    showErrorMessage(userOne);
+}
+
+function checkAccessAndShowMessage (user, role) {
+    if (user.role === role) {
+        showSuccessMessage(userOne);//console.log (`Доступ открыт ${user.firstname}`);  
+    } else {
+        showErrorMessage(userOne);//console.log (`${user.firstname} - Доступ закрыт`);
+    }
+}
+
+checkAccessAndShowMessage (userOne, 'admin');
+checkAccessAndShowMessage (userTwo, 'manager');
